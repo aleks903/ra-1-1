d7827029c3968b43a610bbb03b99c7e9ce6ed419
AppVeyor <ci@appveyor.com>
2020-03-22 16:09:20 +0000

    0038565

---
+++ precache-manifest.3f2e9ac73d490320ca40543018923083.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84323bb3c4d14e442c4df7b479f5b773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55c34203a32d549e51da",
+    "url": "/static/css/main.636d2022.chunk.css"
+  },
+  {
+    "revision": "c29bb24eb16c2942fcce",
+    "url": "/static/js/2.78c28431.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78c28431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c34203a32d549e51da",
+    "url": "/static/js/main.0c8bf755.chunk.js"
+  },
+  {
+    "revision": "229380e47c23cf127752",
+    "url": "/static/js/runtime-main.6d204fcc.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
